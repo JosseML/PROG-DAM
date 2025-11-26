4b66ad64f596de839158f2563cd51f315350bd50
JosseML <jossemv7@gmail.com>
2025-11-26 10:40:57 +0100

    Add files via upload

---
+++ AceptaElReto/Ejercicio21.java
@@ -0,0 +1,16 @@
+package ejercicios;
+
+public class Ejercicio21 {
+
+    public static void main(String[] args) {
+        
+        double a = 2;
+        double b = 4;
+        double c = 7;
+        double media;
+        
+        media = a + b + c / 3;
+        System.out.println(media);
+    }
+
+}
